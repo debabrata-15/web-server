a9b58761318160be124b2fd8f6cdc6b2011f72c3
debabrata-15 <183186061+debabrata-15@users.noreply.github.com>
2026-08-28 00:38:03 +0530

    Added POST and DELETE methods

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import socket
 import json
+import os
 
 SERVER_HOST = "0.0.0.0"
 SERVER_PORT = 8080
@@ -24,15 +25,16 @@ while True:
 
     headers = {}
     for line in header_lines[1:]:
-        if ": " in line :
-            key, value = line.split(": ",1)
+        if ": " in line:
+            key, value = line.split(": ", 1)
             headers[key.lower()] = value
-
-    if http_method == "POST":
+    print(headers)
+    if http_method == "POST" or http_method == "PUT":
         content_length = int(headers.get("content-length", 0))
         while len(body.encode()) < content_length:
             body += client_socket.recv(1024).decode()
 
+    if http_method == "POST":
         filename = ROUTES.get(path)
         if filename:
             try:
@@ -44,10 +46,9 @@ while True:
                 continue
 
             with open(filename, "w") as fout:
-                json.dump(data,fout)
+                json.dump(data, fout)
 
-            response_body = json.dumps({"status": "ok",
-                                        "data": data})
+            response_body = json.dumps({"status": "ok", "data": data})
             response = (
                 "HTTP/1.1 200 OK\r\n"
                 "Content-Type: application/json\r\n"
@@ -65,7 +66,60 @@ while True:
                 response = "HTTP/1.1 404 Not Found\r\n\r\n404 File Not Found"
         else:
             response = "HTTP/1.1 404 Not Found\r\n\r\n404 Page Not Found"
+    elif http_method == "DELETE":
+        filename = ROUTES.get(path)
+        if filename:
+            try:
+                os.remove(filename)
+                response_body = json.dumps(
+                    {"status": "ok", "message": f"{path} deleted"}
+                )
+                response = (
+                    "HTTP/1.1 200 OK \r\n"
+                    "Content-Type: application/json\r\n"
+                    f"Content-Length: {len(response_body)}\r\n"
+                    "\r\n" + response_body
+                )
+            except FileNotFoundError:
+                response_body = json.dumps(
+                    {"status": "error", "message": "Resource Not Found"}
+                )
+                response = (
+                    "HTTP/1.1 404 Not Found\r\n"
+                    "Content-Type: application/json\r\n"
+                    f"Content-Length : {len(response_body)}\r\n"
+                    "\r\n" + response_body
+                )
+        else:
+            response = "HTTP/1.1 404 Not Found\r\n\r\n404 File Not Found"
+    elif http_method == "PUT":
+        filename = ROUTES.get(path)
+        if filename:
+            try:
+                data = json.loads(body)
+            except json.JSONDecodeError:
+                response = "HTTP/1.1 400 Bad Request\r\n\r\nInvalid JSON"
+                client_socket.sendall(response.encode())
+                client_socket.close()
+                continue
+
+            with open(filename, "r+") as file:
+                json_file = json.load(file)
+                for key, value in data.items():
+                    if key in json_file.keys():
+                        json_file[key] = value
+                    json_file[key] = value
+
+            response_body = json.dumps({"status": "ok", "data": data})
+            reponse = response = (
+                "HTTP/1.1 200 OK\r\n"
+                "Content-Type: application/json\r\n"
+                f"Content-Length: {len(response_body)}\r\n"
+                "\r\n" + response_body
+            )
+        else:
+            response = "HTTP/1.1 404 Not Found\r\n\r\n404 File Not Found"
     else:
-        response = "HTTP/1.1 405 Method Not Allowed\n\nAllow: GET"
+        response = "HTTP/1.1 405 Method Not Allowed\n\nAllow: GET,PUT,DELETE,POST"
     client_socket.sendall(response.encode())
     client_socket.close()
